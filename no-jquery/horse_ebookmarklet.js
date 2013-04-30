2db4d7e9fe1a1ea23e7ceefe64b27226ec587809
Ben Nyberg <ben.f.nyberg@gmail.com>
2013-04-30 18:32:54 -0400

    no jquery version almost ready

--- no-jquery/horse_ebookmarklet.js
+++ no-jquery/horse_ebookmarklet.js
@@ -11,6 +11,12 @@
 		jsonScript = document.createElement('script'),
 		tweetArray = [];
 
+	// Append JSON to page which calls getTweets callback.
+	jsonScript.type = 'text/javascript';
+	jsonScript.src = '//api.twitter.com/1/statuses/user_timeline.json?screen_name=horse_ebooks&count=200&callback=getTweets';
+
+	docHead.appendChild(jsonScript);
+
 	var getTweets = function(data) {
 		var urlRegex = /(https?:\/\/[^\s]+)/g,
 			tweetCount = data.length;
@@ -21,131 +27,71 @@
 			//var tweetObject = { text: tweetText, length: tweetText.length };
 			tweetArray.push( tweetText );
 		}
+		// Init the bookmarklet.
 		horseE.init(tweetArray);
 	};
 
-	jsonScript.type = 'text/javascript';
-	jsonScript.src = '//api.twitter.com/1/statuses/user_timeline.json?screen_name=horse_ebooks&count=200&callback=getTweets';
 
-	docHead.appendChild(jsonScript);
 
 	var horseE = {
 		init: function(tweets) {
 			this.tweets = tweets;
-			this.cacheElements(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'a', 'div', 'span']);
+			this.allElements = this.cacheElements(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'a', 'div', 'span']);
+			this.allImages = this.cacheElements(['img']);
 			this.replaceText();
+			this.replaceImages();
 		},
 		cacheElements: function(elements) {
+			// Takes an array of element tags, gets all matching elements 
 			var elementsLength = elements.length;
-			this.allElements = [];
+			var allElements = [];
 			while(elementsLength--) {
 				var nl = document.getElementsByTagName(elements[elementsLength]);
-				for(var i = nl.length; i--; this.allElements.unshift(nl[i]));
+				for(var i = nl.length; i--; allElements.unshift(nl[i]));
 			}
+			return allElements;
 		},
 		replaceText: function() {
 			var eLength = this.allElements.length;
 			for (var i = 0; i < eLength; i++) {
 				var el = this.allElements[i];
-				if (typeof el.text !== 'undefined') {
-					var len = el.text.length;
+				if (typeof el.innerText !== 'undefined') {
+					var len = el.innerText.length;
 					if ((len > 2) && (el.children.length === 0)) {
-						//var replacementText = this.getReplacementTweet(len);
-						//el.text = replacementText;
+						var eligibleTweets = this.tweets.filter(this.checkLength(len));
+						el.innerText = eligibleTweets[Math.floor(Math.random() * eligibleTweets.length)];
 					}
 				}
 			}
 		},
-		checkLength: function(len) {
-			return function(element) {
-				return element.length > len;
-			}
-		}
-	}
-	/*
-	var horseEbookmarklet = {
-		tweetArray: [],
-		init: function() {
-			this.getTweets();
-		},
-		getTweets: function() {
-			// Pull tweets from twitter
-			var that = this;
-			$.getJSON('//api.twitter.com/1/statuses/user_timeline.json?screen_name=horse_ebooks&count=200&callback=?', function (data) {
-				var urlRegex = /(https?:\/\/[^\s]+)/g;
-				var tweetCount = data.length;
-				for (var i = 0; i < tweetCount; i++) {
-					// For all tweets, remove anything matching the URL and put the tweet in a new object along with
-					// it's length. Then put that in the tweetArray.
-					var tweetText = data[i].text.replace(urlRegex, '');
-					var tweetObject = { text: tweetText, length: tweetText.length };
-					that.tweetArray.push( tweetObject );
-				}
-				that.cacheElements();
-				that.replaceText();
-				that.replaceImages();
-			});
-		},
-		cacheElements: function() {
-			// Store all matching elements on the page
-			this.textElements = $('p, h1, h2, h3, h4, h5, h6, li, a, div, span'),
-			this.imgElements = $('img');
-		},
 		replaceImages: function() {
-			this.imgElements.each(function() {
-				// Store each image's width and height
-				var w = $(this).width(),
-					h = $(this).height();
-
-				// Create a new div with that width and height, setting overflow to hidden.
-				// Add the horse image to that div with 100% width and auto height
-				var newImg = $(document.createElement('div')).css({
-					width: w,
-					height: h,
-					overflow: 'hidden',
-					position: 'relative'
-				}).html('<img src="http://www.heyben.com/horse_ebookmarklet/img/horse_ebookmarklet.jpg" alt="horse_ebooks" style="position:absolute; top: 0; left:0;width: 100%; max-width: 100%; min-width: 100%; height: auto;" />');
-				// Replace the images with that div/image
-				$(this).replaceWith(newImg);
-			});
-		},
-		replaceText: function() {
-			var that = this;
-			this.textElements.each(function() {
-				// Get the length of the text in each item.
-				var textLength = $(this).text().length;
-				// Let's ignore elements with only 1 or 2 characters and elements with children
-				if ((textLength > 2) && $(this).children().length === 0) {
-					// Get replacement text
-					var replacementText = that.getReplacementTweet(textLength);
-					$(this).text(replacementText);
+			var eLength = this.allImages.length;
+			for (var i = 0; i < eLength; i++) {
+				var el = this.allImages[i];
+				if ((el.width > 15) && (el.height > 15)){
+					var w = el.width;
+					var h = el.height;
+					var elClass = el.className;
+					var newDiv = document.createElement('div');
+					var newImg = document.createElement('img');
+					newImg.src = 'http://www.heyben.com/horse_ebookmarklet/img/horse_ebookmarklet.jpg';
+					newDiv.className = elClass;
+					newDiv.style.width = w + 'px';
+					newDiv.style.height = h + 'px';
+					newDiv.style.overflow = 'hidden';
+					el.parentNode.replaceChild(newDiv, el);
+					newImg.style.width = '100%';
+					newImg.style.height = 'auto';
+					newDiv.appendChild(newImg);
 				}
-
-			});
+			}
 		},
-		getReplacementTweet: function(length) {
-			// If the text is less than 140 characters, return tweets where the length is within +/-4 characters
-			if (length < 140) {
-				this.greppedTweets = $.grep( this.tweetArray, function(n) {
-					return (n.length > (length -4) && n.length < (length +4));
-				});
-			}
-			// Otherwise if the text is longer than 140 characters, return one that's between 130 & 140 characters
-			else {
-				this.greppedTweets = $.grep( this.tweetArray, function(n) {
-					return (n.length > 130 && n.length < 140);
-				});
-			}
-			// Then from those pick one randomly
-			var tweet = this.greppedTweets[Math.floor(Math.random()*this.greppedTweets.length)];
-			// If it couldn't find a tweet of a similar size, don't return anything, otherwise return replacement text
-			if (typeof tweet === 'undefined') {
-				tweet = false;
-			}
-			return tweet.text;
+		checkLength: function(len) {
+			if (len > 136) { len = 136; }
+			return function(element) {
+				return element.length >= (len - 4) && element.length <= (len + 4);
+			};
 		}
 	};
 
-	horseEbookmarklet.init();
-	*/
 });
